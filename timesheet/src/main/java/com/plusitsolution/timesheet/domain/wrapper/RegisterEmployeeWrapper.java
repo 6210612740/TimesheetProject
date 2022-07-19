6316262e2fb7c07f9617404928c375f419077feb
tharm <tharm@tharm-TUF-Gaming-FX505DT-FX505DT>
2022-07-19 10:56:03 +0700

    create registerEmployeeWrapper

---
+++ timesheet/src/main/java/com/plusitsolution/timesheet/domain/wrapper/RegisterEmployeeWrapper.java
@@ -0,0 +1,84 @@
+package com.plusitsolution.timesheet.domain.wrapper;
+
+import com.plusitsolution.timesheet.domain.TimesheetsEnum.EmpRole;
+
+public class RegisterEmployeeWrapper {
+	
+	private String orgID ;
+	private String username ;
+	private String password ;
+	private String firstName;
+	private String lastName;
+	private String nickName ;
+	private double leaveLimit ;
+	private double medFeeLimit;
+	private String holidayID;
+	private EmpRole empRole ;
+	
+	
+	
+	public String getOrgID() {
+		return orgID;
+	}
+	public void setOrgID(String orgID) {
+		this.orgID = orgID;
+	}
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public String getNickName() {
+		return nickName;
+	}
+	public void setNickName(String nickName) {
+		this.nickName = nickName;
+	}
+	public double getLeaveLimit() {
+		return leaveLimit;
+	}
+	public void setLeaveLimit(double leaveLimit) {
+		this.leaveLimit = leaveLimit;
+	}
+	public double getMedFeeLimit() {
+		return medFeeLimit;
+	}
+	public void setMedFeeLimit(double medFeeLimit) {
+		this.medFeeLimit = medFeeLimit;
+	}
+	public String getHolidayID() {
+		return holidayID;
+	}
+	public void setHolidayID(String holidayID) {
+		this.holidayID = holidayID;
+	}
+	public EmpRole getEmpRole() {
+		return empRole;
+	}
+	public void setEmpRole(EmpRole empRole) {
+		this.empRole = empRole;
+	} 
+	
+	
+	
+
+}
