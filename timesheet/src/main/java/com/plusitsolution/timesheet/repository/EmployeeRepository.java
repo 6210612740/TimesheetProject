dc651b28cab7113cf8b4c2ef219b6bacec12340e
itim1150 <pattaraphon.saowaros@gmail.com>
2022-07-19 11:43:27 +0700

    service regis

--- timesheet/src/main/java/com/plusitsolution/timesheet/repository/EmployeeRepository.java
+++ timesheet/src/main/java/com/plusitsolution/timesheet/repository/EmployeeRepository.java
@@ -1,5 +1,7 @@
 package com.plusitsolution.timesheet.repository;
 
+import java.util.List;
+
 import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,5 +10,9 @@ import com.plusitsolution.timesheet.entity.HolidayEntity;
 
 @Repository
 public interface EmployeeRepository extends ElasticsearchRepository<EmployeeEntity , String> {
+	
+	public List<EmployeeEntity> findAll();
+	
+	public EmployeeEntity findByEmpCode(String empCode);
 
 }
